bd6522d535a0b284619d4d84cd224841cc34f68c
p dung <pdunghn18224@gmail.com>
2024-04-24 03:44:19 +0700

    change root layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,8 +10,19 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={roboto.className}>{children}</body>
+    <html lang="en" className="scroll-smooth">
+      <body className={roboto.className}>
+        <main className="max-w-4xl mx-auto p-4">
+          <AppProvider>
+            <Toaster />
+            <Header />
+            {children}
+            <footer className="border-t p-8 text-center text-gray-500 mt-16">
+              &copy; 2023 All rights reserved
+            </footer>
+          </AppProvider>
+        </main>
+      </body>
     </html>
   );
 }
